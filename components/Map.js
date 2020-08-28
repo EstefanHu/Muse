a458103eea170e46d8cb174e5a01c1a255ac2142
Estefan Hu <estefanhu074@gmail.com>
2020-08-28 16:39:23 -0700

    started context

--- components/Map.js
+++ components/Map.js
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactMapGL from 'react-map-gl'
 
 export default function Map() {
-   const [viewport, setVieport] = React.useState({
-    width: '100vw',
-    height: '100vh',
+  const [viewport, setViewport] = React.useState({
+    width: '100%',
+    height: '100%',
     latitude: 41.5868,
     longitude: -93.625,
     zoom: 13
@@ -15,7 +15,7 @@ export default function Map() {
       {...viewport}
       mapStyle="mapbox://styles/estefan074/ck002rku546481cnq4hc1buof"
       mapboxApiAccessToken="pk.eyJ1IjoiZXN0ZWZhbjA3NCIsImEiOiJja2NiNHV1OXoxYndzMnRsY3VhZWF4YnhjIn0.mSSbeNLgblPKvTs_Wzkxrg"
-      onViewportChange={viewport => setVieport({ viewport })}
+      onViewportChange={viewport => setViewport(viewport)}
     />
   );
 }
